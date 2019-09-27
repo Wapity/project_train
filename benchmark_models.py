21ae62d06bbe114aa226519dfe04311078ceb098
christophe <christophe.silhouette@gmail.com>
2019-09-27 10:49:18 +0200

    preparation pour grid search et creation d un fichier paths.py non suivi pour cloisonement des paths en fonction des users

--- benchmark_models.py
+++ benchmark_models.py
@@ -4,11 +4,16 @@ from models import house_models
 
 from utils.list import value_index_max
 
-def benchmark_models(X_train, X_test, y_train, y_test, models):
-    scores = []
-    for model in models :
-        model.fit(X_train,y_train)
-        scores.append(model.score(X_test, y_test))
+def benchmark_models(X_train, X_test, y_train, y_test, models, tuning = False):
+    if not tuning :
+        scores = []
+        for model in models :
+            model['model'].fit(X_train,y_train)
+            scores.append(model['model'].score(X_test, y_test))
 
-    index_best_model, score_max = value_index_max(scores)
-    return score_max, models[index_best_model]
+        index_best_model, score_max = value_index_max(scores)
+        return score_max, models[index_best_model]
+
+    else:
+        pass
+        # balancer toute la partie grid search
--- main.py
+++ main.py
@@ -9,22 +9,33 @@ from sklearn.metrics import classification_report
 from loader import df_to_X_y
 from benchmark_models import benchmark_models
 from models import house_models
+from paths import PATH_PRINCETON_DATASET
 
 
-PATH = '/Users/alex/project/project_train/data/princeton-dataset_2019-09-02/'
-
-catalog = pd.read_csv(PATH + 'catalog.csv', nrows = 500)
-
-
-X, y = df_to_X_y(catalog, 1, 0)
-
-X_train, X_test, y_train, y_test = train_test_split(
-   X, y, test_size=0.10, random_state=42, shuffle = True)
 
 if __name__ == '__main__':
 
-    # score, model = benchmark_models(X_train, X_test, y_train, y_test, house_models['svm'][:2])
-    # print(score, model)
+    catalog = pd.read_csv(PATH_PRINCETON_DATASET + 'catalog.csv', nrows = 500)
+
+    X, y = df_to_X_y(catalog, 1, 0)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+       X, y, test_size=0.10, random_state=42, shuffle = True)
+
+    best_score, best_model = benchmark_models(X_train, X_test, y_train, y_test, house_models['svm'], tuning = False)
+    print(best_score, best_model)
+
+
+
+
+
+
+
+
+
+
+
+
 
     # Set the parameters by cross-validation
     tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4], 'C': [1, 10, 100, 1000]},
@@ -32,32 +43,31 @@ if __name__ == '__main__':
 
     scores = ['precision', 'recall']
 
-    for score in scores:
-        print("# Tuning hyper-parameters for %s" % score)
-        print()
+    print("# Tuning hyper-parameters for %s" % 'precision')
+    print()
 
-        clf = GridSearchCV(svm.SVC(), tuned_parameters, cv=5,
-                           scoring='%s_macro' % score)
-        clf.fit(X_train, y_train)
+    clf = GridSearchCV(svm.SVC(), tuned_parameters, cv=5,
+                       scoring='%s_macro' % 'precision')
+    clf.fit(X_train, y_train)
 
-        print("Best parameters set found on development set:")
-        print()
-        print(clf.best_params_)
-        print()
-        print("Grid scores on development set:")
-        print()
-        means = clf.cv_results_['mean_test_score']
-        stds = clf.cv_results_['std_test_score']
-        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-            print("%0.3f (+/-%0.03f) for %r"
-                  % (mean, std * 2, params))
-        print()
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
+    print()
 
-        print("Detailed classification report:")
-        print()
-        print("The model is trained on the full development set.")
-        print("The scores are computed on the full evaluation set.")
-        print()
-        y_true, y_pred = y_test, clf.predict(X_test)
-        print(classification_report(y_true, y_pred))
-        print()
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
+    print()
